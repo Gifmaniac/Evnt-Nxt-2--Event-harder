ce2c646050da7e2ca33c459c55b2ebddbd596fce
Gifmaniac <w.vd.b@live.nl>
2025-06-17 11:26:59 +0200

    Revert "Made the try catches on the front end"
    This reverts commit 0fa15c0378110c99ca59b32051a34209f30ef7fa.

--- Evnt-Nxt2/Pages/Index.cshtml.cs
+++ Evnt-Nxt2/Pages/Index.cshtml.cs
@@ -1,9 +1,13 @@
+using Evnt_Nxt_Business_;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace Evnt_Nxt2.Pages;
 
 public class IndexModel : PageModel
 {
+
     public void OnGet()
     {
 
